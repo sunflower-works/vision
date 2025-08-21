8e39cf6ef6fb1066ae9bb6b61d27edc0d85bc11f
beb <elkhatabibilal@gmail.com>
2025-08-21 02:56:06 +0100

    Add eye package with Frame proto definition and generated Go output

---
+++ pkg/vision/eye/frame.pb.go
@@ -0,0 +1,7 @@
+//go:build neve
+// +build neve
+
+// Excluded placeholder: generated from frame.proto via go:generate.
+// Do not edit manually. See generate.go for regeneration instructions.
+
+package eye
