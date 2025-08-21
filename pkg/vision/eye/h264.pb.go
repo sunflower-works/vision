70b35daa41cd68fd7a5461a5298e6356fab05731
beb <elkhatabibilal@gmail.com>
2025-08-21 02:56:21 +0100

    Add H264 proto definition and generated Go output to eye package

---
+++ pkg/vision/eye/h264.pb.go
@@ -0,0 +1,7 @@
+//go:build never
+// +build never
+
+// Excluded placeholder: generated from h264.proto via go:generate.
+// Do not edit manually. See generate.go for regeneration instructions.
+
+package eye
