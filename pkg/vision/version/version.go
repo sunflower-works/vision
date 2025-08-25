3ab7340761e9548ab6b1570163736bf587ce8532
bilalobe <elkhatabibilal@gmail.com>
2025-08-25 21:40:07 +0100

    chore(version): make `Version` variable to allow override at build time via ldflags

--- pkg/vision/version/version.go
+++ pkg/vision/version/version.go
@@ -1,5 +1,6 @@
 package version
 
 // Version is the current module version following SemVer.
+// It is a variable (not const) so it can be overridden at build time via -ldflags "-X .../version.Version=vX.Y.Z".
 // Bump this when cutting a new release and update CHANGELOG.md accordingly.
-const Version = "v0.1.0"
+var Version = "v0.1.0"
